5c585b428904e8162f05b2596eb75a9d42214b10
shchanovlv <shchanovlv@mail.ru>
2020-12-13 00:09:09 +0300

    setup od the express.js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
